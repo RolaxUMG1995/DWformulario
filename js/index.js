87201cdfc08b7858ad79de168ef985504b019ca1
RolaxUMG1995 <rlopezc17@miumg.edu.gt>
2023-07-30 20:34:22 -0600

    Agregando selectores en HTML y JS

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-console.log('Hola mundo');
+const inputText = document.querySelectorAll(".Input-Form input[type='text']");
+const submitButton = document.getElementById("submitButton");
+console.log(submitButton);
+
+
+
+function sendForm(e){
+    e.preventDefault();
+    console.log("Si esta.");
+}
+
